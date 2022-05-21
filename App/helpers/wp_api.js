aed2f7f8b2364124909ecc47da2a9eb166435447
Jorge Peraza <jorgeph2013@gmail.com>
2022-05-21 15:17:11 -0700

    Corrigiendo el dominio

--- App/helpers/wp_api.js
+++ App/helpers/wp_api.js
@@ -1,5 +1,5 @@
 /* const NAME = "localhost:4000/"; */  /* Aqui va el nombre del dominio */
-const NAME = "proyectsweb.github.io/zavan-spa";  /* Aqui va el nombre del dominio */
+const NAME = "proyectsweb.github.io/zavan-spa/";  /* Aqui va el nombre del dominio */
 const DOMAIN = `http://${NAME}`; /* Aqui se puede convinar de acuerdo al dominio completo */
 const SITE = `${DOMAIN}`;
 const API = `${SITE}articulos`;   /* Con esta variable extraigo los datos de los articulos */
@@ -8,6 +8,7 @@ const POST = `${API}/posts`;
 const SEARCH = `${API}/search?_embed&search=`;   /* Para implementar un buscador e implementar enpoints */
 
 console.log(NAME)
+console.log(DOMAIN)
 export {
     NAME,
     DOMAIN,
