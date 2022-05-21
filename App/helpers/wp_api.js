a29a355d817bd97110d9dc2f478c4c933b7ef634
Jorge Peraza <jorgeph2013@gmail.com>
2022-05-21 00:37:29 -0700

    Cambiendo la ruta principal

--- App/helpers/wp_api.js
+++ App/helpers/wp_api.js
@@ -1,5 +1,5 @@
 /* const NAME = "localhost:4000/"; */  /* Aqui va el nombre del dominio */
-const NAME = "proyectsweb.github.io/zavan-spa/";  /* Aqui va el nombre del dominio */
+const NAME = "proyectsweb.github.io/zavan-spa";  /* Aqui va el nombre del dominio */
 const DOMAIN = `http://${NAME}`; /* Aqui se puede convinar de acuerdo al dominio completo */
 const SITE = `${DOMAIN}`;
 const API = `${SITE}articulos`;   /* Con esta variable extraigo los datos de los articulos */
